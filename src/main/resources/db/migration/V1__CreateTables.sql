1ed6cdb79e178f5e1502e650effe871716a49d71
Yuyan <845058547@qq.com>
2019-11-23 13:03:38 +0800

    数据库迁移错误

--- src/main/resources/db/migration/V1__CreateTables.sql
+++ src/main/resources/db/migration/V1__CreateTables.sql
@@ -1,5 +1,3 @@
-
-
 create table user(
 id int primary key auto_increment,
 username varchar(20) not null unique,
@@ -8,6 +6,3 @@ avatar varchar(100),
 create_time datetime,
 modify_time datetime
 )
-
-insert into user(id,username,password,avatar,create_time,modify_time)
-values (1,"zhangsan","123",null,now(),now())
