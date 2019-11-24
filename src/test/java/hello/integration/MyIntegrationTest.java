2c4344c9b9d46aa90df9fdceef91d18ffea51ad0
Yuyan <845058547@qq.com>
2019-11-24 09:06:05 +0800

    搭建和使用JenKins

--- src/test/java/hello/integration/MyIntegrationTest.java
+++ src/test/java/hello/integration/MyIntegrationTest.java
@@ -1,11 +1,25 @@
 package hello.integration;
 
+import hello.Application;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.inject.Inject;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
 public class MyIntegrationTest {
 
+    @Inject
+    Environment environment;
     @Test
-    public void test() {
-
+    public void indexHtmlCanAccessTest() {
+        //拿到应用的端口
+        environment.getProperty("local.service.port");
     }
 }
--- target/classes/db/migration/V1__CreateTables.sql
+++
@@ -1,8 +0,0 @@
-create table user(
-id int primary key auto_increment,
-username varchar(20) not null unique,
-password varchar(100),
-avatar varchar(100),
-create_time datetime,
-modify_time datetime
-)
